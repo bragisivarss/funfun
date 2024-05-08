00c39c26a2e2a440bac771225570d3532f2b3b8c
bragisivarss <sigursteinnbragi@gmail.com>
2024-05-08 14:50:27 +0000

    drinks api

--- src/app/components/DrinksContainer.tsx
+++ src/app/components/DrinksContainer.tsx
@@ -1,4 +1,6 @@
+"use client";
 import Image from "next/image";
+import { useCallback, useEffect, useState } from "react";
 
 {
     /*"use client";
@@ -10,7 +12,7 @@ export const SelectDrinks = () => {
     const [drinks, setDrinks] = useState(null);
 
     useEffect(() => {
-        fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+        fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Beer")
             .then((response) => response.json())
             .then((data) => {
                 console.log(data);
@@ -49,6 +51,19 @@ export const SelectDrinks = () => {
                         <ul>
                             {ingredients.map((item, index) => {
                                 return (
+    useEffect(() => {
+        fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Beer")
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                setDrinks(data.drinks);
+                console.log(drinks);
+            });
+    }, [drinks]);
+
+    if (!drinks) {
+        return <div>Loading...</div>;
+    }
                                     <li className="ingredient" key={index}>
                                         {item}
                                     </li>
@@ -69,29 +84,69 @@ import sprite from "../assets/sprite.png";
 import vodka from "../assets/vodka.png";
 import fanta from "../assets/fanta.png";
 import corona from "../assets/corona.png";
+import axios from "axios";
+
+type Drink = {
+    idDrink: string;
+    strDrink: string;
+    strDrinkThumb: string;
+};
+
 
 export const SelectDrinks = () => {
+    const [drinks, setDrinks] = useState<Drink[]>();
+
+    const fetchDrinks = useCallback(async () => {
+        try {
+            const { data } = await axios.get(
+                "https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Beer"
+            );
+            setDrinks(data.drinks);
+        } catch (err) {}
+        console.log(drinks);
+    }, []);
+
+    useEffect(() => {
+        fetchDrinks();
+    }, [fetchDrinks]);
+
+    if (!drinks) {
+        return <div>Loading...</div>;
+    }
+
     return (
         <>
             <div className="drinks_wrapper">
                 <div className="drinks_container">
                     <div className="single_drink">
-                        <Image src={coke} alt="Coca cola" height={200} />
+                        <Image src={drinks[0].strDrinkThumb} alt={drinks[0].strDrink} width={200} height={200} />
                     </div>
                     <div className="single_drink">
-                        <Image src={fanta} alt="Fanta" height={200} />
+                        <Image src={drinks[1].strDrinkThumb} alt={drinks[1].strDrink} width={200} height={200} />
                     </div>
                     <div className="single_drink">
-                        <Image src={sprite} alt="Sprite" height={200} />
+                        <Image src={drinks[2].strDrinkThumb} alt={drinks[2].strDrink} width={200} height={200} />
                     </div>
                     <div className="single_drink">
-                        <Image src={stella} alt="Gull" height={200} />
+                        <Image src={drinks[3].strDrinkThumb} alt={drinks[3].strDrink} width={200} height={200} />
                     </div>
                     <div className="single_drink">
-                        <Image src={vodka} alt="Smirnoff" height={200} />
+                        <Image src={drinks[4].strDrinkThumb} alt={drinks[4].strDrink} width={200} height={200} />
                     </div>
                     <div className="single_drink">
-                        <Image src={corona} alt="Corona" height={200} />
+                        <Image src={drinks[5].strDrinkThumb} alt={drinks[5].strDrink} width={200} height={200} />
+                    </div>
+                    <div className="single_drink">
+                        <Image src={drinks[6].strDrinkThumb} alt={drinks[6].strDrink} width={200} height={200} />
+                    </div>
+                    <div className="single_drink">
+                        <Image src={drinks[7].strDrinkThumb} alt={drinks[7].strDrink} width={200} height={200} />
+                    </div>
+                    <div className="single_drink">
+                        <Image src={drinks[8].strDrinkThumb} alt={drinks[8].strDrink} width={200} height={200} />
+                    </div>
+                    <div className="single_drink">
+                        <Image src={drinks[9].strDrinkThumb} alt={drinks[9].strDrink} width={200} height={200} />
                     </div>
                 </div>
             </div>
