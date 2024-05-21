75291d16267a468c1cf37c3e6889ac78a1511f34
bragisivarss <sigursteinnbragi@gmail.com>
2024-05-21 10:07:18 +0000

    feat: show ingredients of meal after searching

--- example-api/src/types.ts
+++ example-api/src/types.ts
@@ -7,6 +7,26 @@ export type Provision = {
 
 export type Dish = Provision & {
     strMealThumb: string;
+    strIngredient1: string;
+    strIngredient2: string;
+    strIngredient3: string;
+    strIngredient4: string;
+    strIngredient5: string;
+    strIngredient6: string;
+    strIngredient7: string;
+    strIngredient8: string;
+    strIngredient9: string;
+    strIngredient10: string;
+    strIngredient11: string;
+    strIngredient12: string;
+    strIngredient13: string;
+    strIngredient14: string;
+    strIngredient15: string;
+    strIngredient16: string;
+    strIngredient17: string;
+    strIngredient18: string;
+    strIngredient19: string;
+    strIngredient20: string;
 }
 
 export type Drink = Provision & {
@@ -23,3 +43,4 @@ export type Order = {
     people: number;
     date: Date;
 };
+
